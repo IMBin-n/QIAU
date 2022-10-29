0c7faebb3a1835e237edbaec4f7b2cadd16c726a
IMBin-n <115714588+IMBin-n@users.noreply.github.com>
2022-10-29 22:57:50 +0330

    Add files via upload

---
+++ main 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std ;
+/* Tamrin 1st fahrenheit_to_celsius */
+int main () {
+
+  int F ;
+  float C ;
+  float D;
+  cout << "Enter the Fahrenheit temperature :";
+  cin>> F ;
+   C=F * 5;
+   D= C/9 -32 ;
+           cout << "Celsius temperature: " << D << endl;
+
+    return 0;
+}
